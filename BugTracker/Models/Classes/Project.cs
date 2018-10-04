f79c6246d60b331a8a6c8fec0180e42bc79ad314
Anmol-Singh <anmoldhillon999@gmail.com>
2018-10-04 08:54:38 -0500

    new commit

---
+++ BugTracker/Models/Classes/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Models.Classes
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Project()
+        {
+            Users = new HashSet<ApplicationUser>();
+        }
+
+        public virtual ICollection<ApplicationUser> Users { get; set; }
+    }
+}
