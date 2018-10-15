912923e3710977530f8d541e43be55ed303e1241
Anmol-Singh <anmoldhillon999@gmail.com>
2018-10-15 08:44:36 -0500

    lastc ommit

--- BugTracker/Migrations/Configuration.cs
+++ BugTracker/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace BugTracker.Migrations
 {
+    using BugTracker.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,10 +17,50 @@ namespace BugTracker.Migrations
 
         protected override void Seed(BugTracker.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+            if (!context.Roles.Any(p => p.Name == "Admin"))
+            {
+                var role = new IdentityRole("Admin");
+
+                roleManager.Create(role);
+            }
+
+            if (!context.Roles.Any(p => p.Name == "Project Manager"))
+            {
+                roleManager.Create(new IdentityRole("Project Manager"));
+            }
+
+            if (!context.Roles.Any(p => p.Name == "Developer"))
+            {
+                roleManager.Create(new IdentityRole("Developer"));
+            }
+
+            if (!context.Roles.Any(p => p.Name == "Submitter"))
+            {
+                roleManager.Create(new IdentityRole("Submitter"));
+            }
+
+            ApplicationUser adminUser;
+
+            if (!context.Users.Any(p => p.UserName == "admin@bugtracker.com"))
+            {
+                adminUser = new ApplicationUser();
+                adminUser.Email = "admin@bugtracker.com";
+                adminUser.UserName = "admin@bugtracker.com";
+
+                userManager.Create(adminUser, "Password-1");
+            }
+            else
+            {
+                adminUser = context.Users.First(p => p.UserName == "admin@bugtracker.com");
+            }
+
+            if (!userManager.IsInRole(adminUser.Id, "Admin"))
+            {
+                userManager.AddToRole(adminUser.Id, "Admin");
+            }
         }
     }
 }
